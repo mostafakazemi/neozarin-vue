c82f75e2c6b49c9cb0bbcc2beb641163dda65b18
Mostafa Kazemi <mostafa.kazeminejhad@gmail.com>
2024-01-30 23:50:40 +0330

    Implement social section

---
+++ src/components/SocialSection/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="flex">
+    <div class="flex items-center gap-x-1">
+      <IconLogo />
+      <span>نئوزرین</span>
+    </div>
+    <div class="mr-auto flex gap-x-10">
+      <IconLinkedin />
+      <IconTwitter />
+      <IconInstagram />
+    </div>
+  </section>
+</template>
+
+<script>
+import IconLogo from '@/components/Icons/Logo/index.vue'
+import IconLinkedin from '@/components/Icons/Linkedin/index.vue'
+import IconTwitter from '@/components/Icons/Twitter/index.vue'
+import IconInstagram from '@/components/Icons/Instagram/index.vue'
+
+export default {
+  name: 'SocialSection',
+  components: { IconInstagram, IconTwitter, IconLinkedin, IconLogo }
+}
+</script>
--- src/components/TheFooter/index.vue
+++ src/components/TheFooter/index.vue
@@ -2,15 +2,17 @@
   <footer class="bg-white py-20 px-6 flex flex-col gap-y-6">
     <SupportSection />
     <CertificateSection class="border-b py-6" />
+    <SocialSection />
   </footer>
 </template>
 
 <script>
 import SupportSection from '@/components/SupportSection/index.vue'
 import CertificateSection from '@/components/CertificateSection/index.vue'
+import SocialSection from '@/components/SocialSection/index.vue'
 
 export default {
   name: 'TheHeader',
-  components: { CertificateSection, SupportSection }
+  components: { SocialSection, CertificateSection, SupportSection }
 }
 </script>
