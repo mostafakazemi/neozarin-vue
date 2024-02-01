5053cf6342652f14b2e55cd4f1a94887acbf361b
Mostafa Kazemi <mostafa.kazeminejhad@gmail.com>
2024-02-01 20:28:54 +0330

    Update hero-section styles

--- src/components/HeroSection/index.vue
+++ src/components/HeroSection/index.vue
@@ -1,23 +1,25 @@
 <template>
   <HomeSectionWrapper class="text-center">
-    <h1>
-      شعبه بانکداری
-      <span class="text-primary">مجازی</span>
-      زرین‌پال با نئوزرین
-    </h1>
-    <p class="mt-5 mb-10">
-      نئوزرین سرویسی امن برای انجام امور مالی است که به تازگی توسط زرین‌پال، اولین پرداخت‌یاز دارای
-      مجوز رسمی از بانک مرکزی، عرضه شده است. با نئو بانک زرین، بانکداری الکترونیک و ارائه خدماتی
-      منحصر به فرد برای کاربران زرین‌پال به سادگی فراهم شده است.
-    </p>
-    <div class="flex justify-center">
-      <TheButton class="gap-x-2" size="lg">
-        دانلود رایگان
-        <IconArrow />
-      </TheButton>
-    </div>
-    <div class="mt-16 flex justify-center">
-      <img src="@/assets/images/hero-image.svg" alt="نئوزرین" />
+    <div class="flex flex-col items-center gap-y-5">
+      <h1>
+        شعبه بانکداری
+        <span class="text-primary">مجازی</span>
+        زرین‌پال با نئوزرین
+      </h1>
+      <p class="mb-5">
+        نئوزرین سرویسی امن برای انجام امور مالی است که به تازگی توسط زرین‌پال، اولین پرداخت‌یاز
+        دارای مجوز رسمی از بانک مرکزی، عرضه شده است. با نئو بانک زرین، بانکداری الکترونیک و ارائه
+        خدماتی منحصر به فرد برای کاربران زرین‌پال به سادگی فراهم شده است.
+      </p>
+      <div class="flex justify-center">
+        <TheButton class="gap-x-2" size="lg">
+          دانلود رایگان
+          <IconArrow />
+        </TheButton>
+      </div>
+      <div class="mt-16 flex justify-center">
+        <img src="@/assets/images/hero-image.svg" alt="نئوزرین" />
+      </div>
     </div>
   </HomeSectionWrapper>
 </template>
