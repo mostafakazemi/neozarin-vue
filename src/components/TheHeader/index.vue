c2194c63a27d2a40cb0dd32172b4ba42d7418c4d
Mostafa Kazemi <mostafa.kazeminejhad@gmail.com>
2024-02-01 17:22:35 +0330

    Add logo to header

--- src/components/TheHeader/index.vue
+++ src/components/TheHeader/index.vue
@@ -1,13 +1,14 @@
 <template>
   <header class="flex border-b">
     <div class="mx-auto w-full max-w-screen-xl py-3 px-4 flex justify-between">
-      <div class="flex gap-x-2">
-        <span>logo</span>
-        <span class="font-black text-lg">نئوزرین</span>
+      <div>
+        <img src="@/assets/images/neozarin_logo.svg" alt="نئوزرین" />
       </div>
       <div class="flex gap-x-2">
-        <button class="font-bold">راه‌های ارتباطی</button>
-        <TheButton>دانلود نئوزرین</TheButton>
+        <button class="font-bold" :class="{ 'hidden lg:block': !isInDownloadPage }">
+          راه‌های ارتباطی
+        </button>
+        <TheButton v-if="!isInDownloadPage">دانلود نئوزرین</TheButton>
       </div>
     </div>
   </header>
@@ -18,7 +19,14 @@ import TheButton from '@/components/uikit/TheButton/index.vue'
 
 export default {
   name: 'TheFooter',
-  components: { TheButton }
+
+  components: { TheButton },
+
+  computed: {
+    isInDownloadPage() {
+      return this.$route.name === 'download'
+    }
+  }
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,14 @@ const router = createRouter({
           component: HomeView
         },
         {
+          path: '/download',
+          name: 'download',
+          // route level code-splitting
+          // this generates a separate chunk (About.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () => import('@/pages/About.vue')
+        },
+        {
           path: '/about',
           name: 'about',
           // route level code-splitting
