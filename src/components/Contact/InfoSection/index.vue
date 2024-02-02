c16420afc49750e33011416bd137394a93fceb03
Mostafa Kazemi <mostafa.kazeminejhad@gmail.com>
2024-02-02 21:22:10 +0330

    Create contact-info-section

---
+++ src/components/Contact/InfoSection/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <HomeSectionWrapper>
+    <div class="bg-white rounded-xl p-10 flex flex-col gap-y-10">
+      <InfoSectionItem>
+        <template #icon><IconEmail /></template>
+        <template #title>ایمیل</template>
+        neozarin.support@gmail.com
+      </InfoSectionItem>
+      <InfoSectionItem>
+        <template #icon><IconCall /></template>
+        <template #title>تلفن</template>
+        ۰۲۱-۴۵۶۲۸۴۵۰
+      </InfoSectionItem>
+      <InfoSectionItem>
+        <template #icon><IconTimeProfile /></template>
+        <template #title>ساعات کاری</template>
+        شنبه تا چهارشنبه از ساعت ۹ صبح الی ۵ بعد از ظهر
+      </InfoSectionItem>
+      <InfoSectionItem>
+        <template #icon><IconMarker /></template>
+        <template #title>نشانی</template>
+        تهران، یوسف آباد، خیابان بیستم، پلاک 61، طبقه همکف
+        <br />
+        کد پستی: ۱۴۳۱۹۶۵۳۷۱
+      </InfoSectionItem>
+    </div>
+  </HomeSectionWrapper>
+</template>
+
+<script>
+import HomeSectionWrapper from '@/components/Home/Section/index.vue'
+import IconCall from '@/components/Icons/Call/index.vue'
+import IconEmail from '@/components/Icons/Email/index.vue'
+import InfoSectionItem from '@/components/Contact/InfoSection/Item/index.vue'
+import IconTimeProfile from '@/components/Icons/TimeProfile/index.vue'
+import IconMarker from '@/components/Icons/Marker/index.vue'
+
+export default {
+  name: 'ContactInfoSection',
+  components: {
+    IconMarker,
+    IconTimeProfile,
+    InfoSectionItem,
+    IconEmail,
+    IconCall,
+    HomeSectionWrapper
+  }
+}
+</script>
--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -2,16 +2,18 @@
   <div class="flex flex-col gap-y-32">
     <ContactHeroSection />
     <ContactTicketSection />
+    <ContactInfoSection />
   </div>
 </template>
 
 <script>
 import ContactHeroSection from '@/components/Contact/HeroSection/index.vue'
 import ContactTicketSection from '@/components/Contact/TicketSection/index.vue'
+import ContactInfoSection from '@/components/Contact/InfoSection/index.vue'
 
 export default {
   name: 'ContactPage',
 
-  components: { ContactTicketSection, ContactHeroSection }
+  components: { ContactInfoSection, ContactTicketSection, ContactHeroSection }
 }
 </script>
