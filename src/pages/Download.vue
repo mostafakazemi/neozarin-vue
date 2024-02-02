4ddc08206f194d2f0ee7d162446a46b743ea9936
Mostafa Kazemi <mostafa.kazeminejhad@gmail.com>
2024-02-02 19:30:25 +0330

    Add features-section to download page

--- src/pages/Download.vue
+++ src/pages/Download.vue
@@ -2,15 +2,17 @@
   <div class="flex flex-col gap-y-32">
     <DownloadHeroSection />
     <AppsSection />
+    <FeaturesSection />
   </div>
 </template>
 
 <script>
 import DownloadHeroSection from '@/components/Download/HeroSection/index.vue'
 import AppsSection from '@/components/Download/AppsSection/index.vue'
+import FeaturesSection from '@/components/FeaturesSection/index.vue'
 
 export default {
   name: 'DownloadPage',
-  components: { AppsSection, DownloadHeroSection }
+  components: { FeaturesSection, AppsSection, DownloadHeroSection }
 }
 </script>
